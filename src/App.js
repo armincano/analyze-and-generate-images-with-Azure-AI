35963db6b70145eedb272dc420dd56602929706c
Armin Cano <arminescano@gmail.com>
2023-11-29 21:12:44 +0100

    feat: design GUI in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState } from "react";
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`${inputValue} was Submitted!`);
+    setInputValue(''); // Clear the input field after submission
+  };
+
+  return (
+    <div>
+      <h1>Computer Vision</h1>
+      <p>Insert URL to analyze image, or type a prompt to generate an image:</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          input:
+          <input type="text" value={inputValue} onChange={handleInputChange}/>
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
