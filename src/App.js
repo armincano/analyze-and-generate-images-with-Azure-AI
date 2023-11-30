474cd770fb0747bd34cb495f1dcf4da08a0853cd
Armin Cano <arminescano@gmail.com>
2023-11-30 12:32:46 +0100

    feat: Add image analysis capabilities

--- src/App.js
+++ src/App.js
@@ -1,31 +1,47 @@
 import React, { useState } from "react";
+import analyzeImage from "./utils/azure-image-analysis";
 
 function App() {
-  const [inputValue, setInputValue] = useState('');
+  const loadingScreen = (
+		<div>
+			<h1>Not image data</h1>
+		</div>
+	);
+	const [inputValue, setInputValue] = useState("");
+	const [analysisResult, setAnalysisResult] = useState(loadingScreen);
 
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
-  };
+	const handleInputChange = (e) => {
+		setInputValue(e.target.value);
+	};
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    alert(`${inputValue} was Submitted!`);
-    setInputValue(''); // Clear the input field after submission
-  };
+  const displayedData = (resp) => {
+    return <div>
+      <img src={inputValue} alt="input" />
+      <pre>{JSON.stringify(resp, null, 2)}</pre>
+      </div>
+  }
 
-  return (
-    <div>
-      <h1>Computer Vision</h1>
-      <p>Insert URL to analyze image, or type a prompt to generate an image:</p>
-      <form onSubmit={handleSubmit}>
-        <label>
-          input:
-          <input type="text" value={inputValue} onChange={handleInputChange}/>
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  );
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+    const resp = await analyzeImage(inputValue)
+		setAnalysisResult(displayedData(resp));
+	};
+
+	return (
+		<div>
+			<h1>Computer Vision</h1>
+			<p>Insert URL to analyze image, or type a prompt to generate an image:</p>
+			<form onSubmit={handleSubmit}>
+				<label>
+					input:
+					<input type="text" value={inputValue} onChange={handleInputChange} />
+				</label>
+				<button type="submit">Submit</button>
+			</form>
+			<hr></hr>
+			{analysisResult}
+		</div>
+	);
 }
 
 export default App;
---
+++ src/utils/azure-image-analysis.js
@@ -0,0 +1,27 @@
+async function analyzeImage(imageUrl) {
+
+    const key = process.env.REACT_APP_AZURE_COMPUTER_VISION_API_KEY;
+    const endpoint = process.env.REACT_APP_AZURE_COMPUTER_VISION_API_ENDPOINT;
+    const visualFeatures = ["Categories", "Description", "Color", "Objects", "Tags"];
+    const visualFeaturesParam = visualFeatures.join(',');
+    const apiUrl = `${endpoint}/vision/v3.2/analyze?visualFeatures=${visualFeaturesParam}`;
+
+    const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Ocp-Apim-Subscription-Key': key
+        },
+        body: JSON.stringify({ url: imageUrl })
+    });
+
+    if (!response.ok) {
+        throw new Error(`Azure Computer Vision API request failed: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
+
+export default analyzeImage;
